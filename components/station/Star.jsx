56ba294a1ffefb78049ce5c117088757b22592a5
enitial <spam@literecords.com>
2023-08-09 00:25:34 +0100

    star adds station to faves

--- components/station/Star.jsx
+++ components/station/Star.jsx
@@ -1,26 +1,48 @@
 import { useState } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import IconAD from "react-native-vector-icons/AntDesign";
 
-export default Star = () => {
+export default Star = (props) => {
   const [favBtn, setFavBtn] = useState({
     btnColor: "text-slate-100",
     btnIcon: "staro",
   });
-  return (
-    <TouchableOpacity
-      onPress={() =>
-        favBtn.btnColor === "text-slate-100"
-          ? setFavBtn({
-              btnColor: "text-orange-300",
-              btnIcon: "star",
-            })
-          : setFavBtn({
-              btnColor: "text-slate-100",
-              btnIcon: "staro",
-            })
+
+  const onPress = async () => {
+    try {
+      const temp = await AsyncStorage.getItem("fave");
+      const ifExists = await temp;
+
+      if (favBtn.btnColor === "text-slate-100") {
+        setFavBtn({
+          btnColor: "text-orange-300",
+          btnIcon: "star",
+        });
+        if (ifExists) {
+          console.log("props: ", props.props);
+          console.log("ifExists: ", ifExists);
+          let response = await AsyncStorage.getItem("fave");
+          let parse = await JSON.parse(response);
+          parse.unshift(props.props);
+          await AsyncStorage.setItem("fave", JSON.stringify(parse));
+        } else {
+          await AsyncStorage.setItem("fave", JSON.stringify([props.props]));
+        }
+      } else {
+        setFavBtn({
+          btnColor: "text-slate-100",
+          btnIcon: "staro",
+        });
       }
-    >
+      // console.log(props.props);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={onPress}>
       <View id="fav" className={favBtn.btnColor}>
         <Text className={`p-5 ${favBtn.btnColor}`}>
           <IconAD name={favBtn.btnIcon} size={40} />
